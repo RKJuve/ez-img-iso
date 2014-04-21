7900c24c4e7f98e2bfaf6958dcc4643fa8f282ab
Ryan Juve <RKJuve@gmail.com>
2014-04-20 18:05:46 -0700

    z-layering works. also using predrawn tiles

--- public/3d-array-generator.js
+++ public/3d-array-generator.js
@@ -1,3 +1,38 @@
+var Queue =  function(){
+	this.priorityArray = [];
+	this.hash = {
+	};
+};
+
+Queue.prototype.enqueue = function(value, priority) {
+	// if new priority, set up array. else push value onto array
+	if (!this.hash[priority]) {
+		this.hash[priority] = [value];
+	} else {
+		this.hash[priority].push(value);
+	}
+	// if new priority is >= than old max,
+	// add new priority to front of priorityArray,
+	// else add new priority to end of priorityArray and then sort descending
+	if (priority >= this.priorityArray[0]) {
+		this.priorityArray.unshift(priority);
+	} else {
+		this.priorityArray.push(priority);
+		this.priorityArray.sort(function(a,b){
+			return b - a;
+		});
+	}
+};
+
+Queue.prototype.dequeue = function() {
+	// shift value to return from bucket
+	var toReturn = this.hash[this.priorityArray[0]].shift();
+	// shift value off priorityArray
+	this.priorityArray.shift();
+
+	return toReturn;
+};
+
 function Array3d(x,y,z) {
 	var temp = [];
 	for (i = 0; i < x; i++) {
@@ -11,7 +46,7 @@ function Array3d(x,y,z) {
  	for (i = 0; i < x; i++) {
 		for (j = 0; j < y; j++) {
 			for (k = 0; k < z; k++) {
-					temp[i][j].push(1);
+					temp[i][j].push(0);
 			}			
 		}
  	}
@@ -22,14 +57,14 @@ function GenerateWorld(array, opts) {
 	function genHill(num) {
 		for (i = 0; i < num; i++) {
 			var apex = {};
-				apex.x = 6,
-				apex.y = 8,
-				apex.z = 0;
-			console.log(apex);
+				apex.x = 5,
+				apex.y = 5,
+				apex.z = 3;
+			// console.log(apex);
 			for (i = apex.z; i < z; i++) {
-				for (j = (0 - i); j < (1 + i); j++) {
-					for (k = (0 - i); k < (1 + i); k++) {
-						console.log(k + ', ' + j + ', ' + i)
+				for (j = -i; j < (1 + i); j++) {
+					for (k = -i; k < (1 + i); k++) {
+						// console.log(k + ', ' + j + ', ' + i)
 						array[apex.x + k][apex.y + j][i] = 1;
 					}
 				}
@@ -47,8 +82,83 @@ function GenerateWorld(array, opts) {
 }
 
 var startTime = performance.now();
-var newArray = Array3d(16,16,4);
+var newArray = Array3d(9,9,4);
 endTime = performance.now();
-console.log("done!! took %s seconds", ((endTime-startTime)/1000));
+// console.log("done!! took %s seconds", ((endTime-startTime)/1000));
 
 // newArray = GenerateWorld(newArray)
+newArray = [
+	[
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[1,1,1,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[1,1,1,1]
+	],[
+		[0,0,0,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,0]
+	],[
+		[0,0,0,1],
+		[0,0,1,1],
+		[0,1,1,1],
+		[0,1,1,1],
+		[0,1,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,0]
+	],[
+		[0,0,0,1],
+		[0,0,1,1],
+		[0,1,1,1],
+		[1,1,1,1],
+		[0,1,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,0]
+	],[
+		[0,0,0,1],
+		[0,0,1,1],
+		[0,1,1,1],
+		[0,1,1,1],
+		[0,1,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,0]
+	],[
+		[0,0,0,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,1]
+	],[
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1],
+		[0,0,0,1]
+	],[
+		[1,1,1,1],
+		[0,1,1,1],
+		[0,0,1,1],
+		[0,0,0,1],
+		[0,0,0,0],
+		[0,0,0,0],
+		[0,0,0,0],
+		[0,0,0,1]
+	]
+]
--- public/ez-canvas-iso.js
+++ public/ez-canvas-iso.js
@@ -5,108 +5,123 @@
 /////////
 // GLOBALS
 /////////
-var gridSize = 25;
+var gridSize = 20;
 var APP = {};
 APP.Canvases = {};
 
+// set up tile image
+var Tile = new Image();
+	Tile.src = 'tiletest.png';
+
+
+
+
 $('canvas').each(function() {
 	APP.Canvases[this.id] = this;
-	APP.Canvases[this.id].width = $('body').width();
-	APP.Canvases[this.id].height = 500;
-	console.log(this);
+	this.width = 600;
+	this.height = 600;
 })
 
 var canvasCenter = (function(){
-	var x = $('#canvas1').width() / 2;
-	var y = $('#canvas1').height() / 16;
+	var x = $('#c1').width() / 2;
+	var y = $('#c1').height() / 16;
 	return {x: x, y: y}
 })()
 
 function isoToCart(x, y, z) {
-	var X =  -x + y - 2;
-	var Y = 0.5 * (x + y) - (z / 2) + 1;
+	var X =  y - x - 2;
+	var Y = 0.5 * (x + y) + (z / 2) + 1;
 	return { X: X, Y: Y};
 }
-// var i, j;
+function drawTile(x,y,z) {
+	var cartOBJ = isoToCart(x, y, z);
+	var canvas = APP.Canvases['c'+(50-(z-y-x))];
+	console.log(canvas.id);
 
-newArray.forEach(function(el, ind) {
-	i = ind;
-	el.forEach(function(el, ind){
-		j = ind;
-		el.forEach(function(el, ind) {
+	var X = canvasCenter.x + (cartOBJ.X * gridSize),
+	    Y = canvasCenter.y + (cartOBJ.Y * gridSize),
+		x1 = X + gridSize,
+		x2 = X + (gridSize * 2),
+		y1 = Y - (gridSize * 0.5),
+		y2 = Y + (gridSize * 0.5),
+		y3 = Y + gridSize;
+	console.log(X +', '+Y)
+	el = canvas.getContext('2d');
+	
+	el.drawImage(Tile, X, Y);
+	// Old draw method
+	//////////
+	// el.fillStyle = '#006600';
+	// el.strokeStyle = '#005500';
+	// el.lineJoin = 'round';
+	// el.lineWidth = 1.5;
+	// el.beginPath();
+	// el.moveTo(X, Y);
+	// el.lineTo(x1, y1);
+	// el.lineTo(x2, Y);
+	// el.lineTo(x1, y2);
+	// el.closePath();
+	// el.stroke();
+	// el.fill();
 
-			if (el === 1) {
-				var cartOBJ = isoToCart(i, j, ind);
-				var canvas = APP.Canvases['c'+(ind+i+j)];
+	// el.fillStyle = '#926239';
+	// el.strokeStyle = '#423229';
+
+	// el.beginPath();
+	// el.moveTo(X, Y);
+	// el.lineTo(x1, y2);
+	// el.lineTo(x1, y3);
+	// el.lineTo(X, y2);
+	// el.closePath();
+	// el.stroke();
+	// el.fill();
+
+	// // var grd = c1.createLinearGradient(X,Y,(X+gridSize),(Y+gridSize));
+	// // grd.addColorStop(0,"#926239");
+	// // grd.addColorStop(1,"#926200");
+
+	// // el.fillStyle = grd;
+
+	// el.beginPath();
+	// el.moveTo(x1, y2);
+	// el.lineTo(x2, Y);
+	// el.lineTo(x2, y2);
+	// el.lineTo(x1, y3);
+	// el.closePath();
+	// el.stroke();
+	// el.fill();
+
+	// // shading
+	// el.strokeStyle = 'rgba(35, 29, 0, 0.5)';	
+	// el.beginPath();
+	// el.moveTo(x2, y2);
+	// el.lineTo(x2, (y2 - 1));
+	// el.lineTo(x1, (y3 - 1));
+	// el.lineTo(X, (y2 - 1));
+	// el.lineTo(X, y2);
+	// el.lineTo(x1, y3);
+	// el.closePath();
+	// el.stroke();
+}
+
+var queue = new Queue();
+var counter = 0;
 
 
-				var X = canvasCenter.x + (cartOBJ.X * gridSize),
-				    Y = canvasCenter.y + (cartOBJ.Y * gridSize),
-					x1 = X + gridSize,
-					x2 = X + (gridSize * 2),
-					y1 = Y - (gridSize * 0.5),
-					y2 = Y + (gridSize * 0.5),
-					y3 = Y + gridSize;
-
-				el = canvas.getContext('2d');
-				el.fillStyle = '#006600';
-				el.strokeStyle = '#005500';
-				el.lineJoin = 'round';
-				el.lineWidth = 1.5;
-				el.beginPath();
-				el.moveTo(X, Y);
-				el.lineTo(x1, y1);
-				el.lineTo(x2, Y);
-				el.lineTo(x1, y2);
-				el.closePath();
-				el.stroke();
-				el.fill();
-
-				el.fillStyle = '#926239';
-				el.strokeStyle = '#423229';
-
-				el.beginPath();
-				el.moveTo(X, Y);
-				el.lineTo(x1, y2);
-				el.lineTo(x1, y3);
-				el.lineTo(X, y2);
-				el.closePath();
-				el.stroke();
-				el.fill();
-
-				// var grd = c1.createLinearGradient(X,Y,(X+gridSize),(Y+gridSize));
-				// grd.addColorStop(0,"#926239");
-				// grd.addColorStop(1,"#926200");
-
-				// el.fillStyle = grd;
-
-				el.beginPath();
-				el.moveTo(x1, y2);
-				el.lineTo(x2, Y);
-				el.lineTo(x2, y2);
-				el.lineTo(x1, y3);
-				el.closePath();
-				el.stroke();
-				el.fill();
-
-				// shading
-				el.strokeStyle = 'rgba(35, 29, 0, 0.5)';	
-				el.beginPath();
-				el.moveTo(x2, y2);
-				el.lineTo(x2, (y2 - 1));
-				el.lineTo(x1, (y3 - 1));
-				el.lineTo(X, (y2 - 1));
-				el.lineTo(X, y2);
-				el.lineTo(x1, y3);
-				el.closePath();
-				el.stroke();			
-
+Tile.onload = function() {
+// draw newArray as isotiles
+for (i = 0; i < newArray.length; i++) {
+	for (j = 0; j < newArray[0].length; j++) {
+		for (k = 0; k < newArray[0][0].length; k++) {
+			if (newArray[i][j][k] === 1) {
+				counter++;
+				queue.enqueue([i,j,k], (50-(k-j-i)) );
+			}
 		}
-	})
-})
-});
-// var ctx = c.getContext('2d');
-
-//     ctx.fillRect(25,25,100,100);
-//     ctx.clearRect(45,45,60,60);
-//     ctx.strokeRect(50,50,50,50);
+	}
+}
+for (i = counter; i > 0; i--) {
+	var temp = queue.dequeue()
+	drawTile(temp[0],temp[1],temp[2]);	
+}
+}
